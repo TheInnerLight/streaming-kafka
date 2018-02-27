a5a01bbc4d9b0feb3a188220f8d6cb7c7ef02c90
Phil Curzon <phil@novelfs.org>
2018-02-27 01:03:37 +0000

    Added some documentation

--- src/main/scala/org/novelfs/streaming/kafka/KafkaRecord.scala
+++ src/main/scala/org/novelfs/streaming/kafka/KafkaRecord.scala
@@ -2,8 +2,22 @@ package org.novelfs.streaming.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
 
+/**
+  * @param key The key of the header
+  * @param value The value of the header
+  */
 final case class KafkaHeader(key : String, value : Array[Byte])
 
+/**
+  * @param topicPartition The topic and partition this record was received from
+  * @param offset The offset of this record in the kafka partition
+  * @param key The key of the record
+  * @param value The value of the record
+  * @param timestamp The timestamp of the record
+  * @param serializedKeySize The length of the serialised key, if it exists
+  * @param serializedValueSize The length of the serialised value, if it exists
+  * @param headers The headers associated with the record
+  */
 final case class KafkaRecord[K, V] (
     topicPartition : TopicPartition,
     offset : Long,
@@ -19,6 +33,9 @@ object KafkaRecord {
   def apply[K, V](topicPartition: TopicPartition, offset: Long, key: K, value: V): KafkaRecord[K, V] =
     new KafkaRecord(topicPartition, offset, key, value, None, None, None, List.empty)
 
+  /**
+    * Converts the kafka model of Consumer Records to strongly-typed Kafka records
+    */
   def fromConsumerRecord[K, V](consumerRecord: ConsumerRecord[K, V]): KafkaRecord[K, V] = {
     KafkaRecord(
       topicPartition = TopicPartition(consumerRecord.topic(), consumerRecord.partition()),
--- src/main/scala/org/novelfs/streaming/kafka/KafkaSecuritySettings.scala
+++ src/main/scala/org/novelfs/streaming/kafka/KafkaSecuritySettings.scala
@@ -1,7 +1,16 @@
 package org.novelfs.streaming.kafka
 
+/**
+  * @param trustStoreLocation The location of the trust store file
+  * @param trustStorePassword The password for the trust store file
+  */
 case class KafkaEncryptionSettings(trustStoreLocation : String, trustStorePassword : String)
 
+/**
+  * @param keyStoreLocation The location of the key store file
+  * @param keyStorePassword The store password for the key store file
+  * @param keyPassword The password of the private key in the key store file
+  */
 case class KafkaAuthenticationSettings(keyStoreLocation : String, keyStorePassword : String, keyPassword : String)
 
 sealed trait KafkaSecuritySettings
