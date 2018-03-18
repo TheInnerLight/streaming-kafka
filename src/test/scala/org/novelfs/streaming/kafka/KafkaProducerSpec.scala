5fc74965e5c3e4b12439b4dddc482a5e15546f85
Phil Curzon <phil@novelfs.org>
2018-03-18 23:40:46 +0000

    Removed unused imports

--- src/test/scala/org/novelfs/streaming/kafka/KafkaProducerSpec.scala
+++ src/test/scala/org/novelfs/streaming/kafka/KafkaProducerSpec.scala
@@ -1,12 +1,11 @@
 package org.novelfs.streaming.kafka
 
 import cats.effect.IO
-import org.novelfs.streaming.kafka.producer.{KafkaProducer, ProducerRecord}
+import org.novelfs.streaming.kafka.producer.{KafkaProducer}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.apache.kafka.clients.producer.{RecordMetadata, Producer => ApacheKafkaProducer}
-import KafkaSdkConversions._
+import org.apache.kafka.clients.producer.{Producer => ApacheKafkaProducer}
 
 class KafkaProducerSpec extends FlatSpec with Matchers with MockFactory with GeneratorDrivenPropertyChecks with DomainArbitraries  {
   val rawKafkaProducer = mock[ApacheKafkaProducer[String, String]]
@@ -17,5 +16,5 @@ class KafkaProducerSpec extends FlatSpec with Matchers with MockFactory with Gen
     KafkaProducer.cleanupProducer[IO, String, String](kafkaProducer).unsafeRunSync()
   }
 
-  
+
 }
