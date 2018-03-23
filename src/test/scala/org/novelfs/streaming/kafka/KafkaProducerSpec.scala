374c9700e4169ad33c51cfec84e0ac9aa3edfdc5
Phil Curzon <phil@novelfs.org>
2018-03-23 23:42:53 +0000

    Added test of sendRecord

--- src/test/scala/org/novelfs/streaming/kafka/KafkaProducerSpec.scala
+++ src/test/scala/org/novelfs/streaming/kafka/KafkaProducerSpec.scala
@@ -1,14 +1,21 @@
 package org.novelfs.streaming.kafka
 
 import cats.effect.IO
-import org.novelfs.streaming.kafka.producer.{KafkaProducer}
+import org.novelfs.streaming.kafka.producer.{KafkaProducer, ProducerRecord}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.apache.kafka.clients.producer.{Producer => ApacheKafkaProducer}
+import org.apache.kafka.clients.producer.{RecordMetadata, Producer => ApacheKafkaProducer}
+import KafkaSdkConversions._
+import org.apache.kafka.clients
 
 class KafkaProducerSpec extends FlatSpec with Matchers with MockFactory with GeneratorDrivenPropertyChecks with DomainArbitraries  {
-  val rawKafkaProducer = mock[ApacheKafkaProducer[String, String]]
+
+  trait StringApacheKafkaProducer extends ApacheKafkaProducer[String, String] {
+    override def send(record: clients.producer.ProducerRecord[String, String]): java.util.concurrent.Future[RecordMetadata]
+  }
+
+  val rawKafkaProducer = mock[StringApacheKafkaProducer]
   val kafkaProducer = KafkaProducer(rawKafkaProducer)
 
   "cleanupProducer" should "call producer.close()" in {
@@ -16,5 +23,12 @@ class KafkaProducerSpec extends FlatSpec with Matchers with MockFactory with Gen
     KafkaProducer.cleanupProducer[IO, String, String](kafkaProducer).unsafeRunSync()
   }
 
+  "sendRecord" should "call producer.send()" in {
+    forAll { record : ProducerRecord[String, String] =>
+      val apacheRecord = record.toKafkaSdk
+      (rawKafkaProducer.send : org.apache.kafka.clients.producer.ProducerRecord[String, String] => java.util.concurrent.Future[RecordMetadata]) expects(apacheRecord) once()
 
+      KafkaProducer.sendRecord[IO, String, String](record)(kafkaProducer).unsafeRunSync()
+    }
+  }
 }
